fde3979dd2371a177b0efe2c25eb48505bd4ef68
jdkYang <940604949@qq.com>
2020-06-21 12:39:52 +0800

    将计算结果存回局部变量表

---
+++ src/main/java/cn/yangjian/jvm/instruction/loadandstore/store/Dastore.java
@@ -0,0 +1,29 @@
+package cn.yangjian.jvm.instruction.loadandstore.store;
+
+import cn.yangjian.jvm.instruction.Instruction;
+import cn.yangjian.jvm.interpreter.CallSite;
+import cn.yangjian.jvm.interpreter.CodeUtils;
+import cn.yangjian.jvm.interpreter.Interpreter;
+import cn.yangjian.jvm.runtime.JavaClass;
+import cn.yangjian.jvm.runtime.RunTimeEnv;
+import cn.yangjian.jvm.runtime.heap.ArrayFields;
+import cn.yangjian.jvm.runtime.stack.JThread;
+import cn.yangjian.jvm.runtime.stack.JavaFrame;
+import cn.yangjian.jvm.runtime.stack.LocalVars;
+import cn.yangjian.jvm.runtime.stack.OperandStack;
+import cn.yangjian.jvm.runtime.struct.JObject;
+
+public class Dastore implements Instruction {
+    @Override
+    public void execute(RunTimeEnv runTimeEnv, JThread jThread, JavaClass javaClass, CallSite callSite, Interpreter interpreter, CodeUtils code) {
+        JavaFrame javaFrame = jThread.getTopFrame();
+        OperandStack operandStack = javaFrame.operandStack;
+        LocalVars localVars = javaFrame.localVars;
+
+        double value = operandStack.popDouble();
+        int index = operandStack.popInt();
+        JObject arrayObject = operandStack.popJObject();
+        ArrayFields arrayFields = runTimeEnv.javaHeap.arrayContainer.get(arrayObject.offset);
+        arrayFields.putDouble(index, value);
+    }
+}
