02e00e1a1b3c1117cbc1fcab99e677433d468204
jdkYang <940604949@qq.com>
2020-06-21 11:30:50 +0800

    位运算指令

---
+++ src/main/java/cn/yangjian/jvm/instruction/arithmetic/bitwise/Ineg.java
@@ -0,0 +1,19 @@
+package cn.yangjian.jvm.instruction.arithmetic.bitwise;
+
+import cn.yangjian.jvm.instruction.Instruction;
+import cn.yangjian.jvm.interpreter.CallSite;
+import cn.yangjian.jvm.interpreter.CodeUtils;
+import cn.yangjian.jvm.interpreter.Interpreter;
+import cn.yangjian.jvm.runtime.JavaClass;
+import cn.yangjian.jvm.runtime.RunTimeEnv;
+import cn.yangjian.jvm.runtime.stack.JThread;
+
+/**
+ * 取反
+ */
+public class Ineg implements Instruction {
+    @Override
+    public void execute(RunTimeEnv runTimeEnv, JThread jThread, JavaClass javaClass, CallSite callSite, Interpreter interpreter, CodeUtils code) {
+        System.out.println("未实现 " + this.getClass().getName());
+    }
+}
---
+++ src/main/java/cn/yangjian/jvm/instruction/arithmetic/bitwise/Iushr.java
@@ -0,0 +1,16 @@
+package cn.yangjian.jvm.instruction.arithmetic.bitwise;
+
+import cn.yangjian.jvm.instruction.Instruction;
+import cn.yangjian.jvm.interpreter.CallSite;
+import cn.yangjian.jvm.interpreter.CodeUtils;
+import cn.yangjian.jvm.interpreter.Interpreter;
+import cn.yangjian.jvm.runtime.JavaClass;
+import cn.yangjian.jvm.runtime.RunTimeEnv;
+import cn.yangjian.jvm.runtime.stack.JThread;
+
+public class Iushr implements Instruction {
+    @Override
+    public void execute(RunTimeEnv runTimeEnv, JThread jThread, JavaClass javaClass, CallSite callSite, Interpreter interpreter, CodeUtils code) {
+        System.out.println("未实现 " + this.getClass().getName());
+    }
+}
