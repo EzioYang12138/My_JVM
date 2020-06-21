ee556111adffc2b4faf048803ed5bb7d3f30a798
jdkYang <940604949@qq.com>
2020-06-21 12:00:04 +0800

    一元运算符

---
+++ src/main/java/cn/yangjian/jvm/instruction/arithmetic/unary/Iinc.java
@@ -0,0 +1,29 @@
+package cn.yangjian.jvm.instruction.arithmetic.unary;
+
+import cn.yangjian.jvm.instruction.Instruction;
+import cn.yangjian.jvm.interpreter.CallSite;
+import cn.yangjian.jvm.interpreter.CodeUtils;
+import cn.yangjian.jvm.interpreter.Interpreter;
+import cn.yangjian.jvm.runtime.JavaClass;
+import cn.yangjian.jvm.runtime.RunTimeEnv;
+import cn.yangjian.jvm.runtime.stack.JThread;
+import cn.yangjian.jvm.runtime.stack.JavaFrame;
+import cn.yangjian.jvm.runtime.stack.LocalVars;
+import cn.yangjian.jvm.runtime.stack.OperandStack;
+
+/**
+ * 一元运算符类别
+ */
+public class Iinc implements Instruction {
+    @Override
+    public void execute(RunTimeEnv runTimeEnv, JThread jThread, JavaClass javaClass, CallSite callSite, Interpreter interpreter, CodeUtils code) {
+        JavaFrame javaFrame = jThread.getTopFrame();
+        OperandStack operandStack = javaFrame.operandStack;
+        LocalVars localVars = javaFrame.localVars;
+
+        byte localVarIndex = code.consumeU1();
+        byte constValue = code.consumeU1();
+        int localVar = localVars.getIntByIndex(localVarIndex);
+        localVars.putIntByIndex(localVarIndex, localVar + constValue);
+    }
+}
